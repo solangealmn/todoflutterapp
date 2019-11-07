fa9e07fc79145239b1cff74e7c7520aa5307e123
Solange Nalão <solange.nalao@bgmrodotec.com.br>
2019-11-07 13:30:26 -0300

    app finalizado

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:myapptest/models/item.dart';
 
 void main() {
@@ -20,13 +22,15 @@ class MyApp extends StatelessWidget {
 }
 
 class Home extends StatefulWidget {
+  //chamando a minha model globalmente
   var items = new List<Item>();
 
   Home() {
+    //atribuindo tarefas iniciais
     items = [];
-    items.add(Item(title: 'Task 1', done: false));
-    items.add(Item(title: 'Task 2', done: true));
-    items.add(Item(title: 'Task 3', done: false));
+    // items.add(Item(title: 'Task 1', done: false));
+    // items.add(Item(title: 'Task 2', done: true));
+    // items.add(Item(title: 'Task 3', done: false));
   }
 
   @override
@@ -34,15 +38,70 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  //Pegar o valor do campo
+  var newTaskCtrl = TextEditingController();
+
+  void add() {
+    //validando campo vazio
+    if (newTaskCtrl.text.isEmpty) return;
+
+    //alterando o estado e addicionando um item
+    setState(() {
+      widget.items.add(Item(
+        title: newTaskCtrl.text,
+        done: false,
+      ));
+      newTaskCtrl.text = '';
+      save();
+    });
+  }
+
+  void remove(int index) {
+    //remove item alterando o estado
+    setState(() {
+      widget.items.removeAt(index);
+      save();
+    });
+  }
+
+  Future load() async {
+    //chamo o Shared Preferences para guardar as alterações
+    var prefs = await SharedPreferences.getInstance();
+    //passo os meus dados para o Shared Preferences
+    var data = prefs.getString('data');
+    if (data != null) {
+      Iterable decoded = jsonDecode(data); // transformo em lista
+      List<Item> result =
+          decoded.map((xMapString) => Item.fromJson(xMapString)).toList();
+      setState(() {
+        widget.items = result;
+      });
+    }
+  }
+
+  save() async {
+    var prefs = await SharedPreferences.getInstance();
+    await prefs.setString(
+        'data', jsonEncode(widget.items)); // transformo em string
+  }
+
+  //carrego a minha função dentro do construtor da class
+  //isso evita que a função seja disparada toda vez que o componente passar pelo build
+  _HomeState() {
+    load();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: TextFormField(
+          //passando uma variável de controle
+          controller: newTaskCtrl,
           keyboardType: TextInputType.text,
           style: TextStyle(
             color: Colors.white,
-            fontSize: 20,
+            fontSize: 18,
           ),
           decoration: InputDecoration(
             labelText: 'Nova tarefa',
@@ -59,18 +118,44 @@ class _HomeState extends State<Home> {
         itemCount: widget.items.length,
         itemBuilder: (BuildContext ctxt, int index) {
           final item = widget.items[index];
-          return CheckboxListTile(
-            title: Text(item.title),
+          return Dismissible(
+            child: CheckboxListTile(
+              //incluir checkbox
+              title: Text(item.title),
+              value: item.done,
+              onChanged: (value) {
+                // verificando se o check está marcado ou desmarcado
+                setState(() {
+                  item.done = value;
+                  save();
+                });
+                // tipo um console.log
+                print(value);
+              },
+            ),
             key: Key(item.title),
-            value: item.done,
-            onChanged: (value) {
-              setState(() {
-                item.done = value;
-              });
-              print(value);
+            background: Container(
+              color: Colors.red.withOpacity(0.2),
+              child: Align(
+                alignment: Alignment(-0.9, 0.0),
+                child: Icon(
+                  Icons.delete,
+                  color: Colors.red,
+                ),
+              ),
+            ),
+            onDismissed: (diretion) {
+              if (diretion == DismissDirection.startToEnd) print(diretion);
+              remove(index);
             },
           );
         },
+      ),
+      floatingActionButton: FloatingActionButton(
+        //botão de "+" passando a funcão add
+        onPressed: add,
+        child: Icon(Icons.add),
+        backgroundColor: Colors.pink,
       ),
     );
   }
--- lib/models/item.dart
+++ lib/models/item.dart
@@ -1,4 +1,5 @@
 class Item {
+  //criando uma model
   String title;
   bool done;
 
